53d5c5e6682413ae905a9d0c0940674d9f532882
Javier Velazquez <javier@nobox.com>
2014-01-22 20:43:25 -0500

    Initial commit.

---
+++ content/scripts/coffee/app/main.coffee
@@ -0,0 +1,58 @@
+###
+ Main application module
+
+ * Add routes with their respective module, one per route.
+ * If the same module is needed in two views, include the class instance multiple times
+###
+define (require) ->
+    evnt = require('lib/evnt')
+    $ = require('jquery')
+    # Index = require('app/index')
+    require('reveal')
+
+    # Map routes to their modules
+    routes =
+        'index': Index
+
+    ###
+     Class App
+    ###
+    class App
+
+        constructor: ->
+            console.log('Main app module was built without side effects')
+
+        init: ->
+            $(document).ready =>
+                @$body      = $('body')
+                @basePath   = @$body.data('base')
+                @assetsPath = @$body.data('assets')
+                route       = @$body.data('route')
+
+                @foundation()
+                # @events()
+
+                if typeof routes[route] is 'function'
+                    module = new routes[route]()
+                    module.init()
+
+        events: ->
+            $window = $(window)
+            $window.on 'scroll.main', (e) =>
+                window.requestAnimationFrame =>
+                    @disableHover()
+
+        disableHover: ->
+            clearTimeout(timer);
+
+            if not @$body.hasClass('disable-hover')
+                @$body.addClass('disable-hover')
+
+            timer = setTimeout =>
+                @$body.removeClass('disable-hover')
+            , 500
+
+        foundation: ->
+            $(document).foundation 'reveal'
+
+    return new App
--- content/scripts/js/config.js
+++ content/scripts/js/config.js
@@ -10,10 +10,10 @@ require({
         }
     },
     paths: {
-        facebook: '//connect.facebook.net/en_US/all',
-        twitter: '//platform.twitter.com/widgets',
-        gplusone: '//apis.google.com/js/plusone',
-        gmaps: '//maps.googleapis.com/maps/api/js?key=AIzaSyBms843v8qEZAvDRFMlxpYPufx0rWFvvyw&sensor=true',
+        // facebook: '//connect.facebook.net/en_US/all',
+        // twitter: '//platform.twitter.com/widgets',
+        // gplusone: '//apis.google.com/js/plusone',
+        // gmaps: '//maps.googleapis.com/maps/api/js?key=AIzaSyBms843v8qEZAvDRFMlxpYPufx0rWFvvyw&sensor=true',
         foundation: '../../bower_components/foundation/js/foundation',
         reveal: '../../bower_components/foundation/js/foundation/foundation.reveal',
         jquery: '../../bower_components/jquery/jquery',
@@ -22,24 +22,24 @@ require({
         'normalize-css': '../../bower_components/normalize-css/normalize.css',
         requirejs: '../../bower_components/requirejs/require',
         'require-css': '../../bower_components/require-css/css',
-        sifter: '../../bower_components/sifter/sifter',
+        // sifter: '../../bower_components/sifter/sifter',
         'jquery.finger': '../../bower_components/jquery.finger/dist/jquery.finger',
         mathsass: '../../bower_components/mathsass/dist/math.scss',
         scut: '../../bower_components/scut/dist/_scut.scss',
-        bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap',
-        selectize: '../../bower_components/selectize/dist/js/selectize',
+        // bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap',
+        // selectize: '../../bower_components/selectize/dist/js/selectize',
         async: '../../bower_components/requirejs-plugins/src/async',
         depend: '../../bower_components/requirejs-plugins/src/depend',
         font: '../../bower_components/requirejs-plugins/src/font',
-        goog: '../../bower_components/requirejs-plugins/src/goog',
+        // goog: '../../bower_components/requirejs-plugins/src/goog',
         image: '../../bower_components/requirejs-plugins/src/image',
         json: '../../bower_components/requirejs-plugins/src/json',
-        mdown: '../../bower_components/requirejs-plugins/src/mdown',
-        noext: '../../bower_components/requirejs-plugins/src/noext',
-        propertyParser: '../../bower_components/requirejs-plugins/src/propertyParser',
-        'Markdown.Converter': '../../bower_components/requirejs-plugins/lib/Markdown.Converter',
+        // mdown: '../../bower_components/requirejs-plugins/src/mdown',
+        // noext: '../../bower_components/requirejs-plugins/src/noext',
+        // propertyParser: '../../bower_components/requirejs-plugins/src/propertyParser',
+        // 'Markdown.Converter': '../../bower_components/requirejs-plugins/lib/Markdown.Converter',
         text: '../../bower_components/requirejs-plugins/lib/text',
-        leaflet: '//cdn.leafletjs.com/leaflet-0.7.1/leaflet'
+        // leaflet: '//cdn.leafletjs.com/leaflet-0.7.1/leaflet'
     },
     shim: {
         foundation: {
@@ -52,27 +52,10 @@ require({
                 'foundation'
             ]
         },
-        facebook: {
-            exports: 'FB'
-        },
-        twitter: {
-            exports: 'twttr'
-        },
-        gplusone: {
-            exports: 'gapi.plusone'
-        },
-        gmaps: {
-            exports: 'google'
-        },
-        bootstrap: {
-            deps: [
-                'jquery'
-            ]
-        },
-        'jquery.finger': {
-            deps: [
-                'jquery'
-            ]
-        }
+        // 'jquery.finger': {
+        //     deps: [
+        //         'jquery'
+        //     ]
+        // }
     }
 }, ['start']);
